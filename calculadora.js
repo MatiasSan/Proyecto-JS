cd1bc8bc1962ee2966700a9c7ea23fd24306c38b
Matías <m.coleman.villar@gmail.com>
2020-10-08 20:02:47 -0300

    Calculando

--- calculadora.js
+++ calculadora.js
@@ -1,57 +1,97 @@
-//Intento de calculadora
+function interes() {
+    var montoIngresado;
+    var interesFalabella;
+    var interesRipley;
+    var interesPAris;
+    var cantidadDeCuotas;
 
-class Calculadora {
-   constructor(casa_comercial, nCuotas, montoCompra) {
-       this.casa_comercial = casa_comercial;
-       this.nCuotas = nCuotas;
-       this.montoCompra = montoCompra;
-   } 
+
+
+
+
+
+    //Validar monto de la compra
+    montoIngresado = parseInt(document.getElementById("monto").value);
+    if (document.getElementById("monto").value == '') {
+        alert('Debe ingresar un valor');
+        return;
+    }
+
+    if (Number.isNaN(montoIngresado)) {
+        alert('Debe ingresar un monto');
+        return;
+    }
+
+    if (montoIngresado < 0) {
+        alert('Debe ingresar un monto superior a 0');
+        return;
+    }
+
+    //Validar la casa comercial
+    var casaComercial = document.getElementById("casasComerciales");
+    var casaSeleccionada;
+    if (casaComercial.options[casaComercial.selectedIndex].value == '0') {
+        alert('Debe seleccionar una casa comercial');
+        return;
+    }
+    casaSeleccionada = parseInt(casaComercial.options[casaComercial.selectedIndex].value);
+
+    //Validar numero de cuotas
+    var numeroDeCuotasSeleccionada;
+    var cuotaSeleccionada = document.getElementById("cuotas");
+    if (cuotaSeleccionada.options[cuotaSeleccionada.selectedIndex].value == '0') {
+        alert('Debe seleccionar una cantidad de cuotas');
+        return;
+    }
+    numeroDeCuotasSeleccionada = parseInt(cuotaSeleccionada.options[cuotaSeleccionada.selectedIndex].value);
+
+    //Calcular monto total
+    var montoTotal = 0;
+    var valorFinalCuota = 0;
+
+    interesFalabella = 1.05;
+    interesRipley = 1.06;
+    interesPAris = 1.08;
+
+    //aqui se opera en base al valor de select "casasComerciales"
+    
+    switch (casaSeleccionada) {
+        case 1: //Falabella
+            montoTotal = montoIngresado * interesFalabella;
+            break;
+        case 2: //Ripley
+            montoTotal = montoIngresado * interesRipley;
+            break;
+        case 3: //Paris
+            montoTotal = montoIngresado * interesPAris;
+            break;
+        default:
+            break;
+    }
+
+    // Calculo cuota mensual
+
+    switch (numeroDeCuotasSeleccionada) {
+        case 1:
+            valorFinalCuota = parseInt(cuotaSeleccionada) / 1; 
+            break;
+        case 2:
+            valorFinalCuota = parseInt(cuotaSeleccionada) / 2;
+            break;
+        case 3:
+            valorFinalCuota = parseInt(cuotaSeleccionada) / 3;
+            break;
+        default:
+            break;
+    }
+
+    console.log(montoTotal)
+    document.getElementById("montoFinal").innerHTML = montoTotal;
+
+    //Calcular el valor de la cuota
+
+    console.log(valorFinalCuota);
+    document.getElementById("cuotaMensual").innerHTML = valorFinalCuota;
+
+    return;
 }
-
-
-
-
-
-
-
-
-
-//Proto objeto
-
-
-
-/* class Calculadora {
-    TASA_INTERES;
-    constructor(casa_comercial, nCuotas, montoCompra){
-        this.casa_comercial = casa_comercial;
-        this.nCuotas = nCuotas;
-        this.montoCompra = montoCompra;
-        this.getTasaInteres();
-    }
-    getTasaInteres = function(){
-        if(this.casa_comercial === 'falabella'){
-            this.TASA_INTERES = 1.44;
-        } else if(this.casa_comercial === 'paris'){
-            this.TASA_INTERES = 1.66;
-        } else if(this.casa_comercial === 'ripley'){
-            this.TASA_INTERES === 1.80;
-        } else{
-            console.log("La casa comercial ingresa no existe");
-        }
-    }
-    calcularCuotaMes = function(){
-        return  (this.montoCompra + (this.TASA_INTERES * this.nCuotas * this.montoCompra / 100)) / this.nCuotas
-    }
-    montototalapagar = function(){
-        return (this.montoCompra + (this.TASA_INTERES * this.nCuotas * this.montoCompra / 100));
-    }
-}
-
-
-let cal1 = new Calculadora('falabella', 12, 12000); */
-
-
-  //numero de cuotas
-  //casa comercial
-  //monto de compra
-  //Tasa de interes????
